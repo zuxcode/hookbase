2de48f6265acde85d23931a3cbfc32c591613413
Alfred Nwanokwai <codeauthor1@gmail.com>
2026-09-06 14:58:18 +0100

    Feat: release setup

--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -2,12 +2,7 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   dts: true,
-  entry: [
-    "src/index.ts",
-    "src/presets-arktype.ts",
-    "src/presets-zod.ts",
-    "src/presets-valibot.ts",
-  ],
+  entry: ["src/**/*.ts", "src/**/*.tsx"],
   platform: "neutral",
   unbundle: true,
 });
